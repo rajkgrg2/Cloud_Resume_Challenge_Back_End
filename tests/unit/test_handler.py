8298e58c01a0daa3d766ca47b9889472bc09bd70
rajkgrg2 <sum40@hotmail.com>
2022-11-09 22:41:29 -0500

    Update test_handler.py

--- tests/unit/test_handler.py
+++ tests/unit/test_handler.py
@@ -1,65 +1,113 @@
+import os
+import re
 import json
 
-import pytest
+from unittest import mock
+import sys
+import os.path
+sys.path.append(
+    os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
+from backend import app
 
-from hello_world import app
+with open('resume-app/template.yaml', 'r') as f:
+    TABLENAME = re.search(r'TableName: (.*)?', f.read()).group(1)
+
+@mock.patch.dict(os.environ, {"TABLENAME": TABLENAME})
+def test_lambda_handler():
+    # Check AWS creds
+    assert "AWS_ACCESS_KEY_ID" in os.environ
+    assert "AWS_SECRET_ACCESS_KEY" in os.environ
+
+    ret = app.lambda_handler("", "")
+
+    # Assert return keys
+    assert "statusCode" in ret
+    assert "headers" in ret
+    assert "body" in ret
+
+    # Check for CORS in Headers
+    assert "Access-Control-Allow-Origin"  in ret["headers"]
+    assert "Access-Control-Allow-Methods" in ret["headers"]
+    assert "Access-Control-Allow-Headers" in ret["headers"]
+
+    # Check status code
+    if ret["statusCode"] == 200:
+        assert "visit_count" in ret["body"]
+        assert json.loads(ret["body"])["visit_count"].isnumeric()
+    else:
+        assert json.loads(ret["body"])["visit_count"] == -1
+
+    return
 
 
-@pytest.fixture()
-def apigw_event():
-    """ Generates API GW Event"""
 
-    return {
-        "body": '{ "test": "body"}',
-        "resource": "/{proxy+}",
-        "requestContext": {
-            "resourceId": "123456",
-            "apiId": "1234567890",
-            "resourcePath": "/{proxy+}",
-            "httpMethod": "POST",
-            "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
-            "accountId": "123456789012",
-            "identity": {
-                "apiKey": "",
-                "userArn": "",
-                "cognitoAuthenticationType": "",
-                "caller": "",
-                "userAgent": "Custom User Agent String",
-                "user": "",
-                "cognitoIdentityPoolId": "",
-                "cognitoIdentityId": "",
-                "cognitoAuthenticationProvider": "",
-                "sourceIp": "127.0.0.1",
-                "accountId": "",
-            },
-            "stage": "prod",
-        },
-        "queryStringParameters": {"foo": "bar"},
-        "headers": {
-            "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
-            "Accept-Language": "en-US,en;q=0.8",
-            "CloudFront-Is-Desktop-Viewer": "true",
-            "CloudFront-Is-SmartTV-Viewer": "false",
-            "CloudFront-Is-Mobile-Viewer": "false",
-            "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
-            "CloudFront-Viewer-Country": "US",
-            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
-            "Upgrade-Insecure-Requests": "1",
-            "X-Forwarded-Port": "443",
-            "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
-            "X-Forwarded-Proto": "https",
-            "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
-            "CloudFront-Is-Tablet-Viewer": "false",
-            "Cache-Control": "max-age=0",
-            "User-Agent": "Custom User Agent String",
-            "CloudFront-Forwarded-Proto": "https",
-            "Accept-Encoding": "gzip, deflate, sdch",
-        },
-        "pathParameters": {"proxy": "/examplepath"},
-        "httpMethod": "POST",
-        "stageVariables": {"baz": "qux"},
-        "path": "/examplepath",
-    }
+
+
+
+
+
+# import json
+
+# import pytest
+
+# from hello_world import app
+
+
+# @pytest.fixture()
+# def apigw_event():
+#     """ Generates API GW Event"""
+
+#     return {
+#         "body": '{ "test": "body"}',
+#         "resource": "/{proxy+}",
+#         "requestContext": {
+#             "resourceId": "123456",
+#             "apiId": "1234567890",
+#             "resourcePath": "/{proxy+}",
+#             "httpMethod": "POST",
+#             "requestId": "c6af9ac6-7b61-11e6-9a41-93e8deadbeef",
+#             "accountId": "123456789012",
+#             "identity": {
+#                 "apiKey": "",
+#                 "userArn": "",
+#                 "cognitoAuthenticationType": "",
+#                 "caller": "",
+#                 "userAgent": "Custom User Agent String",
+#                 "user": "",
+#                 "cognitoIdentityPoolId": "",
+#                 "cognitoIdentityId": "",
+#                 "cognitoAuthenticationProvider": "",
+#                 "sourceIp": "127.0.0.1",
+#                 "accountId": "",
+#             },
+#             "stage": "prod",
+#         },
+#         "queryStringParameters": {"foo": "bar"},
+#         "headers": {
+#             "Via": "1.1 08f323deadbeefa7af34d5feb414ce27.cloudfront.net (CloudFront)",
+#             "Accept-Language": "en-US,en;q=0.8",
+#             "CloudFront-Is-Desktop-Viewer": "true",
+#             "CloudFront-Is-SmartTV-Viewer": "false",
+#             "CloudFront-Is-Mobile-Viewer": "false",
+#             "X-Forwarded-For": "127.0.0.1, 127.0.0.2",
+#             "CloudFront-Viewer-Country": "US",
+#             "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+#             "Upgrade-Insecure-Requests": "1",
+#             "X-Forwarded-Port": "443",
+#             "Host": "1234567890.execute-api.us-east-1.amazonaws.com",
+#             "X-Forwarded-Proto": "https",
+#             "X-Amz-Cf-Id": "aaaaaaaaaae3VYQb9jd-nvCd-de396Uhbp027Y2JvkCPNLmGJHqlaA==",
+#             "CloudFront-Is-Tablet-Viewer": "false",
+#             "Cache-Control": "max-age=0",
+#             "User-Agent": "Custom User Agent String",
+#             "CloudFront-Forwarded-Proto": "https",
+#             "Accept-Encoding": "gzip, deflate, sdch",
+#         },
+#         "pathParameters": {"proxy": "/examplepath"},
+#         "httpMethod": "POST",
+#         "stageVariables": {"baz": "qux"},
+#         "path": "/examplepath",
+#     }
 
 
 def test_lambda_handler(apigw_event):
